c2469cb5460b1922980716c74a0fd2dbd57e4603
xianghongai <xianghongai@gmail.com>
2024-10-18 16:34:35 +0800

    feat: Status Bar add 'Toggle Panel'

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 'use strict';
-import { commands, ExtensionContext } from 'vscode';
+import { commands, ExtensionContext, window, StatusBarAlignment } from 'vscode';
 
 import {
   copyPath,
@@ -19,14 +19,14 @@ import {
   ACTION_TYPE,
 } from './functions/copy-case';
 
-export function activate(context: ExtensionContext) {
-  context.subscriptions.push(
+export function activate({ subscriptions }: ExtensionContext) {
+  subscriptions.push(
     commands.registerCommand('opened-editors.formatDocument', () => {
       commands.executeCommand('editor.action.formatDocument');
     })
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.revealSidebar', () => {
       commands.executeCommand(
         'workbench.files.action.showActiveFileInExplorer'
@@ -34,160 +34,165 @@ export function activate(context: ExtensionContext) {
     })
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.openedEditors', () => {
       commands.executeCommand('workbench.action.showAllEditors');
     })
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.copyPath', copyPath)
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.copyFolderName', copyFolderName)
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.copyFileName', copyFileName)
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand(
       'opened-editors.copyFileNameWithExtension',
       copyFileNameWithExtension
     )
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand(
       'opened-editors.fileTreeGenerator',
       fileTreeGenerator
     )
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.fold', foldHandler)
   );
 
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('opened-editors.unfold', unfoldHandler)
   );
 
   // Copy Case
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.commands', () => quickCaseAction(ACTION_TYPE.copy))
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.constant', () => {
       caseAction(ACTIONS.constant, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.camel', () => {
       caseAction(ACTIONS.camel, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.pascal', () => {
       caseAction(ACTIONS.pascal, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.kebab', () => {
       caseAction(ACTIONS.kebab, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.snake', () => {
       caseAction(ACTIONS.snake, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.dot', () => {
       caseAction(ACTIONS.dot, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.path', () => {
       caseAction(ACTIONS.path, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.title', () => {
       caseAction(ACTIONS.title, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.lower', () => {
       caseAction(ACTIONS.lower, ACTION_TYPE.copy);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('copy-case.upper', () => {
       caseAction(ACTIONS.upper, ACTION_TYPE.copy);
     })
   );
 
   // Paste Case
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.commands', () => quickCaseAction(ACTION_TYPE.paste))
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.constant', () => {
       caseAction(ACTIONS.constant, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.camel', () => {
       caseAction(ACTIONS.camel, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.pascal', () => {
       caseAction(ACTIONS.pascal, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.kebab', () => {
       caseAction(ACTIONS.kebab, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.snake', () => {
       caseAction(ACTIONS.snake, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.dot', () => {
       caseAction(ACTIONS.dot, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.path', () => {
       caseAction(ACTIONS.path, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.title', () => {
       caseAction(ACTIONS.title, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.lower', () => {
       caseAction(ACTIONS.lower, ACTION_TYPE.paste);
     })
   );
-  context.subscriptions.push(
+  subscriptions.push(
     commands.registerCommand('paste-case.upper', () => {
       caseAction(ACTIONS.upper, ACTION_TYPE.paste);
     })
   );
+
+  // Status Bar add 'Toggle Panel'
+  const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right);
+  statusBarItem.text = '$(terminal-powershell)';
+  statusBarItem.tooltip = 'Toggle Panel';
+  statusBarItem.command = 'workbench.action.togglePanel';
+  statusBarItem.show();
+  subscriptions.push(statusBarItem);
 }
 
 export function deactivate() {
-  // window.setStatusBarMessage(
-  //   '"Opened Editors" extension disabled',
-  //   3000
-  // );
+  //
 }
